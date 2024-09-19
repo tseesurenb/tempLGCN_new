1cb8574c1fa4c5966a7583c2cd7bb37cc3cd7f0d
tseesurenb <133945855+tseesurenb@users.noreply.github.com>
2024-09-19 11:37:19 +1000

    hi

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
 import numpy as np
 
-rmse = [1, 2, 3, 4, 5]
+data = np.array([1, 20, 13, 14, 15])
 
-print(f"RMSE: {rmse[0]} , {rmse[1]} , {rmse[2]} , {rmse[3]} , {rmse[4]} | {np.mean(rmse)}, {np.std(rmse)}")
+beta = 0.1
+
+p = np.exp(-beta * data)
+
+print(p)
+
